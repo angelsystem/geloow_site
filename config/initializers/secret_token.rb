1f723ed9137031950e03d08cc7821ff42fce16e3
angel roberto <angelsystem1387@gmail.com>
2012-03-24 16:43:51 -0500

    base app geloow

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeloowSite::Application.config.secret_token = '89b1af180097c1c38ba118c0c4aefd97b68495ca899cbd4dc882b7bc7bf815680dafc1a5fe77d49889ce6aa43119a90dcefa8a6d8956c6f9247185f9615dd876'
